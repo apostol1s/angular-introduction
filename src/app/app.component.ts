50afdde1567a5ee0e8e9b519f6786bac25589af4
ApostolosTourlidas <tourlidasapostolis@gmail.com>
2024-04-01 13:53:23 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = "Apostolos";
-
-  person = {
-    givenName: 'Apostolos',
-    surName: 'Tourlidas',
-    age: 27,
-    email: 'tourlidasa@aueb.gr',
-  };
+  name = "Apostolos"; 
 }
