ed4f529f308ec8cc9a78badf62392ed59d73d519
ApostolosTourlidas <tourlidasapostolis@gmail.com>
2024-04-23 15:36:31 +0300

    login user finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { PersonTableComponent } from './components/person-table/person-table.component';
-import { Person } from './shared/interfaces/person';
-import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListGroupMenuComponent } from "./components/list-group-menu/list-group-menu.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterLink, RouterOutlet, ListGroupMenuComponent]
+    imports: [RouterLink, RouterOutlet, ListGroupMenuComponent, NavbarComponent]
 })
 export class AppComponent {}
